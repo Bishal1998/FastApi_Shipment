22e5b461e138a73364859aa86875cc41c8066ccf
Bishal Paudel <bishalpaudel1998@gmail.com>
2026-04-28 22:01:50 -0400

    seller routers updated with access token

--- app/api/routers/seller.py
+++ app/api/routers/seller.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
-from app.api.dependencies import SellerServiceDep, get_access_token
+from app.api.dependencies import SellerServiceDep, get_seller_access_token
 from app.api.schemas.seller import CreateSeller, ReadSeller
 from app.database.redis import add_jti_to_blacklist
 
@@ -28,6 +28,6 @@ async def login(
 
 
 @router.get("/logout")
-async def logout(token_data: Annotated[dict, Depends(get_access_token)]):
+async def logout(token_data: Annotated[dict, Depends(get_seller_access_token)]):
     await add_jti_to_blacklist(token_data["jti"])
     return {"detail": "Logged out successfully"}
