a0e09d0fbbde056c6fa803bf1e90198d3c242fc3
Bishal Paudel <bishalpaudel1998@gmail.com>
2026-04-28 22:01:16 -0400

    oauth2_scheme added for delivery partner

--- app/core/security.py
+++ app/core/security.py
@@ -1,3 +1,4 @@
 from fastapi.security import OAuth2PasswordBearer
 
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl="seller/login")
+oauth2_seller = OAuth2PasswordBearer(tokenUrl="seller/login")
+oauth2_delivery_partner = OAuth2PasswordBearer(tokenUrl="partner/login")
