774c69193d98efc878a732f61e647c6f816ee5fc
Bishal Paudel <bishalpaudel1998@gmail.com>
2026-04-29 23:31:31 -0400

    ShipmentEventService created

---
+++ app/services/shipment_event.py
@@ -0,0 +1,40 @@
+
+from app.database.models import Shipment, ShipmentEvent, ShipmentStatus
+from app.services.base import BaseService
+
+
+class ShipmentEventService(BaseService):
+    def __init__(self, session):
+        super().__init__(ShipmentEvent, session)
+
+    async def add(self, shipment:Shipment, location : int | None = None, status : ShipmentStatus | None = None, description : str | None = None):
+
+        if not location or not status:
+            last_event = await self.get_latest_event(shipment)
+
+            location = location if location else last_event.location
+            status = status if status else last_event.status
+
+        new_event = ShipmentEvent(
+            location = location,
+            status = status,
+            description=description if description else self._generate_description(status, location), 
+            shipment_id=shipment.id
+        )
+        return await self._add(new_event)
+    
+    async def get_latest_event(self, shipment : Shipment):
+       timeline = shipment.timeline
+       timeline.sort(key=lambda event: event.created_at)
+       return timeline[-1]
+    
+    def _generate_description(self, status : ShipmentStatus, location : int):
+        match status:
+            case ShipmentStatus.PLACED:
+                return "asssigned delivery partner"
+            case ShipmentStatus.OUT_FOR_DELIVERY:
+                return "shipment out for delivery"
+            case ShipmentStatus.DELIVERED:
+                return "successfully delivered"
+            case _:
+                return f"Scanned at {location}"
