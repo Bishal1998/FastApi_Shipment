6b1f9fb6a7b8d2c45edb5b78b9f2b6bab0f36b49
Bishal Paudel <bishalpaudel1998@gmail.com>
2026-04-30 22:34:19 -0400

    fastapi-mail setup and tested

--- app/config.py
+++ app/config.py
@@ -29,6 +29,21 @@ class JWTSettings(BaseSettings):
 
     model_config = _base_config
 
+class NotificationSettings(BaseSettings):
+    MAIL_USERNAME:str
+    MAIL_PASSWORD:str
+    MAIL_PORT:int
+    MAIL_SERVER:str
+    MAIL_FROM:str
+    MAIL_FROM_NAME:str
+    MAIL_STARTTLS:bool = True
+    MAIL_SSL_TLS:bool = False
+    USE_CREDENTIALS:bool = True
+    VALIDATE_CERTS:bool = True
+
+    model_config = _base_config
+
 
 settings = DatabaseSettings()
 jwt_settings = JWTSettings()
+notification_settings = NotificationSettings()
