1bc53bc9c0ecd9f294a273c7ba2b6936afbc3043
Bishal Paudel <bishalpaudel1998@gmail.com>
2026-06-09 07:59:23 -0400

    try/except removed to check the logs

--- app/worker/tasks.py
+++ app/worker/tasks.py
@@ -1,3 +1,5 @@
+import logging
+
 from asgiref.sync import async_to_sync
 from celery import Celery
 from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
@@ -6,6 +8,8 @@ from twilio.rest import Client
 
 from app.config import notification_settings, settings, twilio_settings
 from app.utils import TEMPLATE_DIR
+
+logger = logging.getLogger(__name__)
 
 fastmail = FastMail(
     ConnectionConfig(
@@ -57,12 +61,17 @@ def send_sms(to: str, body: str):
 async def send_email_with_template(
     recipients: list[EmailStr], subject: str, context: dict, template_name: str
 ):
-    await fastmail.send_message(
-        message=MessageSchema(
-            recipients=recipients,
-            subject=subject,
-            template_body=context,
-            subtype=MessageType.html,
-        ),
-        template_name=template_name,
-    )
+    try:
+        await fastmail.send_message(
+            message=MessageSchema(
+                recipients=recipients,
+                subject=subject,
+                template_body=context,
+                subtype=MessageType.html,
+            ),
+            template_name=template_name,
+        )
+        logger.info("EMAIL SENT SUCCESSFULLY")
+    except Exception as e:
+        logger.error(f"EMAIL ERROR: {type(e).__name__}: {e}")
+        raise
